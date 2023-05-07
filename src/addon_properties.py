9a298e8a04d460298137f606cd3506aae759e229
industArk <achoruzy@gmail.com>
2023-05-07 18:56:55 +0200

    updated pip dir name

--- src/addon_properties.py
+++ src/addon_properties.py
@@ -16,7 +16,7 @@ from bpy.props import (StringProperty,
 class ScripterProperties(bpy.types.PropertyGroup):
     
     pip_lib_dir: StringProperty(
-        name="pip_lib_dir",
+        name="PIP library directory",
         description="Directory to store installed python libraries.",
         default=os.path.expanduser("~") + "/.scripter/lib",
         maxlen=1024,
