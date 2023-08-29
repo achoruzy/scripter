f78075842394cb554a69f9c6c220ee4dd59992ef
industArk <achoruzy@gmail.com>
2023-08-29 22:31:32 +0200

    implemented using operators to handle main logic

--- src/properties_ops.py
+++ src/properties_ops.py
@@ -3,39 +3,66 @@
 
 import bpy
 
-from .data import PYPI_libraries
+from .scripter import Pypi_Handler
 
 
-default = 'New library name'
+name_text = 'New library name'
 
 class SCRIPTER_OT_pypi_list_add(bpy.types.Operator):
 	bl_idname = "scripter.pypi_add"
 	bl_label = "SCRIPTER_OT_pypi_list_add"
-	bl_description = "Add new PYPI library name for installation."
+	bl_description = "Add new PYPI library name to list."
 
-	new_lib: bpy.props.StringProperty(default= default, name = default)
+	lib_name: bpy.props.StringProperty(default= "", name = name_text)
+
+	pypi: Pypi_Handler = None
 
 	def invoke(self, context, event):
 		return context.window_manager.invoke_props_dialog(self, width=200)
 
 	def execute(self, context):
-		global PYPI_libraries
-		if self.new_lib != default and self.new_lib != "":
-			PYPI_libraries.add(self.new_lib)
-		print(PYPI_libraries)
-
+		if self.lib_name != name_text and self.lib_name != "":
+			self.pypi.add(self.lib_name)
+		
 		return{'FINISHED'}
+	
+	@classmethod
+	def initialize_with(cls, pypi_handler):
+		cls.pypi = pypi_handler
+		return cls
 
 
 class SCRIPTER_OT_pypi_list_remove(bpy.types.Operator):
 	bl_idname = "scripter.pypi_remove"
 	bl_label = "SCRIPTER_OT_pypi_list_remove"
-	bl_description = "Remove PYPI library and uninstall."
+	bl_description = "Remove PYPI library from list."
+
+	pypi: Pypi_Handler = None
 
 	def execute(self, context):
-		global PYPI_libraries
-		print(PYPI_libraries)
-		PYPI_libraries.remove("pandas")
-		print(PYPI_libraries)
+		self.pypi.remove("pandas")
 
 		return{'FINISHED'}
+	
+	@classmethod
+	def initialize_with(cls, pypi_handler):
+		cls.pypi = pypi_handler
+		return cls
+
+
+class SCRIPTER_OT_pypi_list_update(bpy.types.Operator):
+	bl_idname = "scripter.pypi_update"
+	bl_label = "SCRIPTER_OT_pypi_list_update"
+	bl_description = "Update installed libraries with current list"
+
+	pypi: Pypi_Handler = None
+
+	def execute(self, context):
+		self.pypi.update_packages()
+
+		return{'FINISHED'}
+	
+	@classmethod
+	def initialize_with(cls, pypi_handler):
+		cls.pypi = pypi_handler
+		return cls
